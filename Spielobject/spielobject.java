a26357e6422de3bfe2f01a9bab91cf736b68b72f
Simon Dahmen <SD>
2022-11-14 14:28:31 +0100

    withInit

--- Spielobject/spielobject.java
+++ Spielobject/spielobject.java
@@ -1,6 +1,11 @@
 package Spielobject;
 
 public class spielobject {
-    String [] [] Farbenmatrix = new String [10] [10];
-     
+    String [] [] Farbenmatrix = new String [4] [10];
+    String [] [] ZielArray = new String [4] [10];
+    public static String[][] Init()
+    {
+        String [] [] ZielArray = new String [4] [10];
+        return ZielArray;
+    }
 }
