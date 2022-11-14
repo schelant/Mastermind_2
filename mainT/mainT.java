e24ac4bcebb3dd32e41e30142fba02fc69f664eb
Simon Dahmen <SD>
2022-11-14 12:41:32 +0100

    init

---
+++ mainT/mainT.java
@@ -0,0 +1,17 @@
+package mainT;
+
+// abstract class Spieler{
+//     abstract void CreateMap();
+//     abstract void SetShip();
+//     abstract void ShowMaps();
+//     abstract void SeeAndFire();
+// }
+
+public class mainT
+{
+    public static void main (String[] args) 
+    {
+        System.out.println("hello");
+    }
+}
+
