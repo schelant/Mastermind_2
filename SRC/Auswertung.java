acabbf88245d10d6d4652598f7600c4d212927fd
schelant <antoina.schelnberger@smail.emt.h-brs.de>
2022-12-02 19:58:53 +0100

    Attributnamen angepasst, Spielregelausgabe hinzugefügt

--- SRC/Auswertung.java
+++ SRC/Auswertung.java
@@ -4,8 +4,8 @@ public class Auswertung {
     public int[][] positionUndFarbeRichtig;
 
     public void initPositionsmatrix(SpielObjekt x){
-        int [][]PosNumberCorrect = new int[x.anzahlRunden][2];
-        this.positionUndFarbeRichtig = PosNumberCorrect;
+        int [][]posNumberCorrect = new int[x.anzahlRunden][2];
+        this.positionUndFarbeRichtig = posNumberCorrect;
         // hier herrscht ein riesen chaos, ich wusste es nicht besser
         for (int i=0; i<x.anzahlRunden; i++){   // initialisierung?!
             this.positionUndFarbeRichtig[i][0] = 0;
@@ -13,26 +13,26 @@ public class Auswertung {
         }
     }
 
-    public int[][] vergleichenEingabeZiel(String[] ZielArray, String[] EingabeArray, int Rundenzahl){
+    public int[][] vergleichenEingabeZiel(String[] zielArray, String[] eingabeArray, int rundenzahl){
         int i, j;
-        int []allesRichtig = new int[ZielArray.length];
-        int []farbeRichtig = new int[ZielArray.length];
-        int []untersucht = new int[ZielArray.length];
+        int []allesRichtig = new int[zielArray.length];
+        int []farbeRichtig = new int[zielArray.length];
+        int []untersucht = new int[zielArray.length];
         //int []PosNumberCorrect = new int[2];
         
-        for (i = 0; i<EingabeArray.length; i++){
-            if (ZielArray[i].equalsIgnoreCase(EingabeArray[i])){
+        for (i = 0; i<eingabeArray.length; i++){
+            if (zielArray[i].equalsIgnoreCase(eingabeArray[i])){
                 // pos und farbe richtig = 1
                 allesRichtig[i] = 1;
                 untersucht[i] = 1;
             }
         }
 
-        for (i = 0; i<EingabeArray.length; i++){
-            for (j = 0; j<ZielArray.length; j++){
+        for (i = 0; i<eingabeArray.length; i++){
+            for (j = 0; j<zielArray.length; j++){
                 //System.out.println(ZielArray[j]+" : "+EingabeArray[i]);
                 if(untersucht[j] == 0){
-                    if (ZielArray[j].equalsIgnoreCase(EingabeArray[i])){
+                    if (zielArray[j].equalsIgnoreCase(eingabeArray[i])){
                         farbeRichtig[i] = 1;
                         untersucht[j] = 1;
                         // farbe richtig = 2
@@ -43,12 +43,12 @@ public class Auswertung {
             }
         }
 
-        for (i = 0;i<ZielArray.length;i++){
+        for (i = 0;i<zielArray.length;i++){
             if (allesRichtig[i] == 1){
-                this.positionUndFarbeRichtig[Rundenzahl][0]++;
+                this.positionUndFarbeRichtig[rundenzahl][0]++;
             }
             else if (farbeRichtig[i] == 1){
-                this.positionUndFarbeRichtig[Rundenzahl][1]++;
+                this.positionUndFarbeRichtig[rundenzahl][1]++;
             }
         }
         return this.positionUndFarbeRichtig;
