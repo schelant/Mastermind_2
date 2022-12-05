e56dc91d8df2e5c425214aafcb75cbe025463ebe
Simon Dahmen <SD>
2022-12-05 14:04:42 +0100

    forloop

--- SRC/SpielEingabe.java
+++ SRC/SpielEingabe.java
@@ -6,13 +6,17 @@ public class SpielEingabe {
     public String[] eingabeReihe;
 
     public String[] einlesen(Scanner eingabe){
+        for (;true;) {
+            System.out.println("Gib was ein, Freund!");
+            String s = eingabe.nextLine();
+            this.eingabeReihe = s.split(" ");
+            if (this.eingabeReihe.length == 4){
+                break;
+            }else{
+                System.out.println("das ist die falsche anzahl an farben ...");
+            }
+        }
         
-        System.out.println("Gib was ein, Freund!");
-
-        String s = eingabe.nextLine();
-
-        
-        this.eingabeReihe = s.split(" ");
 
         //System.out.println(" Du schriebst: " + eingabe);
 
